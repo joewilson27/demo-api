5850d2c3345891207ddc184ee04d5058c8a3123a
Joe Wilson <andreas.w@barantum.com>
2023-02-15 10:09:29 +0700

    Add jpa, mysql connector, config properties, model class product

---
+++ demo-api/src/main/java/com/domain/models/entities/Product.java
@@ -0,0 +1,69 @@
+package com.domain.models.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_product") // ini akan menjadi nama table di sisi mysql
+public class Product implements Serializable {
+    
+    @Id // jadikan primary key pada table mysql
+    @GeneratedValue(strategy=GenerationType.IDENTITY) // jadikan auto-increment pada sisi mysql
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    
+
+}
